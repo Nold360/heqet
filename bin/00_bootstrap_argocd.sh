753512a1219045149a916311f337391480b68bfc
nold <nold@gnu.one>
2020-05-27 21:18:44 +0200

    fix(heqet)

--- bin/00_bootstrap_argocd.sh
+++ bin/00_bootstrap_argocd.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "Installing ArgoCD..."
-kubectl apply -n argocd -f manifests/argocd.yaml
+helm repo add argo https://argoproj.github.io/argo-helm
+
+kubectl create ns argocd
+helm install argo argo/argo-cd --namespace argocd
+#kubectl apply -n argocd -f manifests/argocd.yaml
 
 echo
-echo "Bootstrapping Heket Apps..."
+echo "Bootstrapping Heqet Apps..."
 kubectl apply -n argocd -f manifests/heqet-apps.yaml
 
 exit 0
--- bin/50_port_forward.sh
+++ bin/50_port_forward.sh
@@ -5,7 +5,7 @@ echo 'Starting Portforwarding...'
 killall kubectl
 sleep 1
 
-kubectl port-forward svc/argocd-server -n argocd 8081:443 &>/dev/null &
+kubectl port-forward svc/argo-argocd-server -n argocd 8081:443 &>/dev/null &
 kubectl port-forward svc/loki-grafana -n heqet-loki 8082:80 &>/dev/null &
 kubectl port-forward svc/heqet-vault-ui -n heqet-vault 8083:8200 &>/dev/null &
 kubectl port-forward  service/heqet-jaeger-jaeger-operator-jaeger-query -n heqet-jaeger 8084:8200 &>/dev/null &
